e6210eb87b1f7334e51b26251fcac9d1625956c7
kadamsagar039 <40513747+kadamsagar039@users.noreply.github.com>
2019-03-25 19:12:37 +0530

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -5,7 +5,8 @@ cd /home/ubuntu/cd-demo/
 source /home/ubuntu/venv/bin/activate
 python3 /home/ubuntu/cd-demo/fundoo/manage.py collectstatic
 pip3 install django bcrypt django-extensions
-pip3 install gunicorn
+sudo service gunicorn restart
+sudo service nginx restart
 
 
 
