89ca568fb12ad3d5deb18809d29e6beeb1b02994
kadamsagar039 <40513747+kadamsagar039@users.noreply.github.com>
2019-03-25 18:56:42 +0530

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt-get install python3-pip python3-dev nginx git
-cd /home/ubuntu/project/
+cd /home/ubuntu/cd-demo/
 source /home/ubuntu/venv/bin/activate
-python3 /home/ubuntu/new_chatapp/manage.py collectstatic
+python3 /home/ubuntu/cd-demo/fundoo/manage.py collectstatic
 pip3 install django bcrypt django-extensions
 pip3 install gunicorn
 
