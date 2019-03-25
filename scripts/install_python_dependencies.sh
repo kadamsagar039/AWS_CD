dd5f99914bb2e748ec4733f3708691dd00fe1b92
kadamsagar039 <40513747+kadamsagar039@users.noreply.github.com>
2019-03-25 19:13:30 +0530

    Update install_python_dependencies.sh

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -5,3 +5,4 @@ chown ubuntu:ubuntu /home/ubuntu/venv
 chown ubuntu:ubuntu /home/ubuntu/venv/*
 source /home/ubuntu/venv/bin/activate
 pip3 install -r /home/ubuntu/cd-demo/requirements/base.txt
+pip3 install gunicorn
