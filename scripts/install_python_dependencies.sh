bb7a25067e7b51231f8aa696f0d1cd3e1ef824f1
kadamsagar039 <40513747+kadamsagar039@users.noreply.github.com>
2019-03-25 19:05:50 +0530

    Update install_python_dependencies.sh

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -4,4 +4,4 @@ virtualenv /home/ubuntu/venv
 chown ubuntu:ubuntu /home/ubuntu/venv
 chown ubuntu:ubuntu /home/ubuntu/venv/*
 source /home/ubuntu/venv/bin/activate
-pip install -r /home/ubuntu/cd-demo/requirements/staging.txt
+pip3 install -r /home/ubuntu/cd-demo/requirements/base.txt
