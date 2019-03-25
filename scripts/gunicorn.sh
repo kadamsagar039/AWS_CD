9735606fa2027b57ca5ad7303ba9d828136d1845
kadamsagar039 <40513747+kadamsagar039@users.noreply.github.com>
2019-03-25 18:52:19 +0530

    Update gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp /home/ubuntu/new_chatapp/nginx/gunicorn.service /etc/systemd/system/
+sudo cp /home/ubuntu/cd-demo/nginx/gunicorn.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
