a4989fd010894fa9aaaaff43b3abc7a8a36a9a37
kadamsagar039 <40513747+kadamsagar039@users.noreply.github.com>
2019-03-26 13:07:49 +0530

    Update install_python_dependencies.sh

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 chown ubuntu:ubuntu /home/ubuntu
-sudo apt-get install python3-pip python3-dev nginx git -y
-pip3 install virtualenv
 virtualenv /home/ubuntu/venv
 chown ubuntu:ubuntu /home/ubuntu/venv
 chown ubuntu:ubuntu /home/ubuntu/venv/*
