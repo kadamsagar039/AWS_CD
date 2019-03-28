c981c5f16edeccaee0afc872c877f34fd5a295b4
kadamsagar039 <40513747+kadamsagar039@users.noreply.github.com>
2019-03-28 18:17:57 +0530

    Update clean_instance.sh

--- scripts/clean_instance.sh
+++ scripts/clean_instance.sh
@@ -3,6 +3,6 @@ rm -rf /home/ubuntu/*
 sudo apt-get update
 sudo apt-get install python3-pip python3-dev nginx git -y
 sudo pip3 install virtualenv
-virtualenv venv
-source venv/bin/activate
+virtualenv /home/ubuntu/venv
+source /home/ubuntu/venv/bin/activate
 
