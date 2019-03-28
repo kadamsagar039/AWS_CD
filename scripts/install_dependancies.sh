1195cfcbc76bfb8600cdebabd7e1b762a28aecb2
kadamsagar039 <40513747+kadamsagar039@users.noreply.github.com>
2019-03-28 19:23:33 +0530

    Update install_dependancies.sh

--- scripts/install_dependancies.sh
+++ scripts/install_dependancies.sh
@@ -5,6 +5,6 @@ sudo apt-get install python3-pip python3-dev nginx git -y
 sudo pip3 install virtualenv
 virtualenv /home/ubuntu/venv
 source /home/ubuntu/venv/bin/activate
-sudo pip3 install -r /home/ubuntu/cd-demo/requirements/base.txt
-sudo pip3 install django bcrypt django-extensions
-sudo pip3 install gunicorn
+sudo /home/ubuntu/venv/bin/pip3 install -r /home/ubuntu/cd-demo/requirements/base.txt
+sudo /home/ubuntu/venv/bin/pip3 install django bcrypt django-extensions
+sudo /home/ubuntu/venv/bin/pip3 install gunicorn
