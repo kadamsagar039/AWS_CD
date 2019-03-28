2ede4266e7895faad5f82d19703b8b8cc4021995
kadamsagar039 <40513747+kadamsagar039@users.noreply.github.com>
2019-03-28 18:02:03 +0530

    Update install_python_dependencies.sh

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -3,7 +3,6 @@
 pip3 install -r /home/ubuntu/cd-demo/requirements/base.txt
 pip3 install django bcrypt django-extensions
 pip3 install gunicorn
-cd cd-demo/fundoo/
-exec gunicorn --bind 0.0.0.0:8000 fundoo.wsgi:application
+
 
 
