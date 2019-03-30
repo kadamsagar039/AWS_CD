23ef8026458630b9751e0a56217d36390cfd5a16
kadamsagar039 <40513747+kadamsagar039@users.noreply.github.com>
2019-03-30 15:18:27 +0530

    Update install_dependancies.sh

--- scripts/install_dependancies.sh
+++ scripts/install_dependancies.sh
@@ -9,6 +9,7 @@ cd /home/ubuntu/cd-demo/
 pip3 install -r requirements.txt
 pip3 install django bcrypt django-extensions
 sudo chown ubuntu:ubuntu /home/ubuntu/cd-demo
+cd /home/ubuntu/cd-demo/fundoo/
 python3 manage.py collectstatic
 # sudo rm -rf /etc/nginx/sites-available/C
 # sudo cp /home/ubuntu/ChatApp-Django/files/ChatApp-Django /etc/nginx/sites-available/
