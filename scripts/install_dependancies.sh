888a46ce4099e6bf6ed743f1897f968f296d781c
kadamsagar039 <40513747+kadamsagar039@users.noreply.github.com>
2019-03-30 14:20:26 +0530

    Update install_dependancies.sh

--- scripts/install_dependancies.sh
+++ scripts/install_dependancies.sh
@@ -5,14 +5,14 @@ sudo apt-get update
 sudo pip3 install virtualenv
 virtualenv --python=python3 /home/ubuntu/venv
 source /home/ubuntu/venv/bin/activate
-cd /home/ubuntu/ChatApp-Django/
+cd /home/ubuntu/cd-demo/
 pip3 install -r requirements.txt
 pip3 install django bcrypt django-extensions
-sudo chown ubuntu:ubuntu /home/ubuntu/ChatApp-Django
+sudo chown ubuntu:ubuntu /home/ubuntu/cd-demo
 python3 manage.py collectstatic
-sudo rm -rf /etc/nginx/sites-available/ChatApp-Django
-sudo cp /home/ubuntu/ChatApp-Django/files/ChatApp-Django /etc/nginx/sites-available/
-sudo rm -rf /etc/nginx/sites-enabled/ChatApp-Django
-sudo ln -s /etc/nginx/sites-available/ChatApp-Django /etc/nginx/sites-enabled/
-sudo nginx -t
-sudo rm -rf /etc/nginx/sites-enabled/default
+# sudo rm -rf /etc/nginx/sites-available/C
+# sudo cp /home/ubuntu/ChatApp-Django/files/ChatApp-Django /etc/nginx/sites-available/
+# sudo rm -rf /etc/nginx/sites-enabled/ChatApp-Django
+# sudo ln -s /etc/nginx/sites-available/ChatApp-Django /etc/nginx/sites-enabled/
+# sudo nginx -t
+# sudo rm -rf /etc/nginx/sites-enabled/default
